cdc5a74075c1d79fe1f8b27c760a2247499f5d13
Mário Feroldi <mferoldif@gmail.com>
2019-12-24 01:00:02 -0300

    Play a little with transactions

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,23 @@ struct BudgetSettings {
     currency_format: CurrencyFormat,
 }
 
+impl Default for BudgetSettings {
+    fn default() -> BudgetSettings {
+        BudgetSettings {
+            date_format: DateFormat("%Y-%m-%d".into()),
+            currency_format: CurrencyFormat {
+                iso_code: CurrencyISOCode::Usd,
+                dec_digits: 2,
+                dec_sep: Separator::Period,
+                symbol_first: true,
+                group_sep: Separator::Comma,
+                currency_symbol: "US$".into(),
+                display_symbol: true,
+            },
+        }
+    }
+}
+
 /// An account holds a currency balance that may be used on a budget.
 struct Account {
     /// A descriptive name for this account.
@@ -40,6 +57,7 @@ struct Account {
     /// The type of account, e.g. whether it's a checking, asset or liability
     /// kind of account.
     kind: AccountKind,
+    // FIXME(feroldi): this is a property of AccountKind, so make this into a function.
     /// Whether this account is on budget or not.
     on_budget: bool,
     /// Whether this account is closed or not.
@@ -101,7 +119,7 @@ enum GoalKind {
     },
     TargetCategoryBalanceByDate {
         target_balance: i64,
-        target_date: Date<Utc>,
+        by_date: Date<Utc>,
     },
     MonthlyFunding {
         funding_balance: i64,
@@ -130,7 +148,7 @@ struct Transaction {
     memo: String,
     cleared: ClearedStatus,
     approved: bool,
-    flag_color: String,
+    flag_color: Option<String>,
 }
 
 enum ClearedStatus {
@@ -139,27 +157,16 @@ enum ClearedStatus {
     Reconciled,
 }
 
-struct Budget {
-    name: String,
-    first_month: Date<Utc>,
-    last_month: Date<Utc>,
-    settings: BudgetSettings,
-    accounts: Vec<Account>,
-    payees: Vec<Payee>,
-    category_groups: Vec<CategoryGroup>,
-    categories: Vec<Category>,
-    months: Vec<Month>,
-    transactions: Vec<Transaction>,
-}
-
 struct Month {
     month: Date<Utc>,
     note: String,
-    /// The total amount in transactions categorized to 'Inflow: To be Budgeted' in the month.
+    /// The total amount in transactions categorized to 'Inflow: To be Budgeted'
+    /// in the month.
     income: i64,
     /// The total amount budgeted in the month.
     budgeted: i64,
-    /// The total amount in transactions in the month, excluding those categorized to 'Inflow: To be Budgeted'.
+    /// The total amount in transactions in the month, excluding those
+    /// categorized to 'Inflow: To be Budgeted'.
     activity: i64,
     /// The available amount for 'To be Budgeted'.
     to_be_budgeted: i64,
@@ -169,6 +176,89 @@ struct Month {
     is_deleted: bool,
 }
 
+struct Budget {
+    name: String,
+    settings: BudgetSettings,
+    accounts: Vec<Account>,
+    payees: Vec<Payee>,
+    category_groups: Vec<CategoryGroup>,
+    categories: Vec<Category>,
+    months: Vec<Month>,
+    transactions: Vec<Transaction>,
+}
+
+impl Budget {
+    fn with_name(name: impl Into<String>) -> Budget {
+        Budget {
+            name: name.into(),
+            settings: BudgetSettings::default(),
+            accounts: vec![],
+            payees: vec![],
+            category_groups: vec![],
+            categories: vec![],
+            months: vec![], // TODO(feroldi): initialize with the current month?
+            transactions: vec![],
+        }
+    }
+
+    fn commit_transaction(&mut self, tx: Transaction) {
+        self.accounts[tx.account.0].balance += tx.amount;
+        match tx.cleared {
+            ClearedStatus::Cleared | ClearedStatus::Reconciled => {
+                self.accounts[tx.account.0].cleared_balance += tx.amount;
+            }
+            ClearedStatus::Uncleared => {
+                self.accounts[tx.account.0].uncleared_balance += tx.amount;
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut budget = Budget::with_name("test");
+
+    budget.accounts.push(Account {
+        name: "a".into(),
+        kind: AccountKind::Checking,
+        on_budget: true,
+        is_closed: false,
+        note: "".into(),
+        balance: 0,
+        cleared_balance: 0,
+        uncleared_balance: 0,
+        is_deleted: false,
+    });
+
+    budget.commit_transaction(Transaction {
+        date: Utc::today(),
+        amount: 200,
+        account: AccountId(0),
+        payee: PayeeId(0),
+        category: CategoryId(0),
+        transfer_account: None,
+        memo: "Description".into(),
+        cleared: ClearedStatus::Cleared,
+        approved: true,
+        flag_color: None,
+    });
+
+    budget.commit_transaction(Transaction {
+        date: Utc::today(),
+        amount: -150,
+        account: AccountId(0),
+        payee: PayeeId(0),
+        category: CategoryId(0),
+        transfer_account: None,
+        memo: "Blabla".into(),
+        cleared: ClearedStatus::Uncleared,
+        approved: true,
+        flag_color: None,
+    });
+
+    let a = &budget.accounts[0];
+
+    println!(
+        "{} {} {}",
+        a.balance, a.cleared_balance, a.uncleared_balance
+    );
 }
